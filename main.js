69cdd2ec05c698d88757fb49520a1875ba62f24f
Lavya5586 <78597821+Lavya5586@users.noreply.github.com>
2021-09-14 19:07:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/iO2jmACXc/',modelLoaded());
+}
+
+function draw()
+{
+    image(video, 0, 0, 300, 300);
+    classifier.classify(video, gotResult());
+}
+
+function modalLoaded()
+{
+    console.log('Model Loaded!');
+}
+
+function gotResult()
+{
+    if (error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
